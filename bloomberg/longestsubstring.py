2ce988ce4b90af51ed1bf8eabaac880a8f6312d5
prondubuisi <onyemenamndu@gmail.com>
2020-10-01 04:27:29 +0100

    Longest sub string brute force

---
+++ bloomberg/longestsubstring.py
@@ -0,0 +1,39 @@
+# Find the length of the longest substring within a sequence without repeating characters
+#example input abccabc
+#returns 3 len(abc)
+# loop through string
+# keep track of start position
+# intialize an empty dictionary 
+# add  characters to dictionary as you loop
+# if an encountered char is already in dict 
+# get length of dict, store as longest
+# continue, starting from start position + len(dict)
+# continue if start position is < len input
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        estart = 0
+        istart = 0
+        limit = len(s)
+        store = []
+        longest = 0
+        while(istart < limit):
+           
+           if s[istart] in store:
+               store = []
+               istart = estart + 1
+               estart += 1 
+               
+           else:
+                store.append(s[istart])
+                istart += 1
+
+           if len(store) > longest:
+                longest = len(store)
+           
+        return longest
+#test position 3
+
+sol = Solution()
+
+print(sol.lengthOfLongestSubstring(" "))
